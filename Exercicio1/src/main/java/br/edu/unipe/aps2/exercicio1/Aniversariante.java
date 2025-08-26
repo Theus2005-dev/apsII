37be5afccad1f0d5bf0b6f0b06f4af12ffdfb04d
Theus2005-dev <matheusarcangelli36@gmail.com>
2025-08-26 14:21:59 -0300

    feature:criação de construtotes Aniversariante.java

--- Exercicio1/src/main/java/br/edu/unipe/aps2/exercicio1/Aniversariante.java
+++ Exercicio1/src/main/java/br/edu/unipe/aps2/exercicio1/Aniversariante.java
@@ -3,8 +3,39 @@ package br.edu.unipe.aps2.exercicio1;
 public class Aniversariante {
 	
 	private String nome;
+	private int dia;
+	private int mes;
 	private DataAniversario dataAniversario;
-	
-	//gets e sets...
 
+	// constructors
+	public Aniversariante(String nome, int dia, int mes){
+		this.nome = nome;
+		this.dia = dia;
+		this.mes= mes;
+	}
+	public Aniversariante(String nome, DataAniversario  dataAniversario){
+		this.nome = nome;
+		this.dataAniversario = dataAniversario;
+	}
+
+
+	//gets e sets...
+	public void setName(String name){
+		this.nome = name;
+	}
+	public void setDia(int dia){
+		this.dia = dia;
+	}
+	public void setMes(int mes){
+		this.mes = mes;
+	}
+	public String getNome(){
+		return this.nome;
+	}
+	public int getDia(){
+		return this.dia;
+	}
+	public int getMes(){
+		return this.mes;
+	}
 }
