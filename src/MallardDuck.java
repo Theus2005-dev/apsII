780936acc8c327babe6a2190a080c1598a1387f4
MATHEUS ARCANGELLI CAVALCANTI SILVA <matheus.silva000@cs.unipe.edu.br>
2025-10-23 09:09:49 -0300

    Implementação do codigo do livro Use a Cabcea p.37-40

---
+++ src/MallardDuck.java
@@ -0,0 +1,17 @@
+public class MallardDuck extends Duck{
+    public MallardDuck(){
+        quackBehavior = new Quack();
+        flyBehavior = new FlyWithWings();
+    }
+    public void setFlyBehavior(FlyBehavior fb){
+        flyBehavior = fb;
+    }
+    public void setQuackBehavior(QuackBehavior qb){
+        quackBehavior = qb;
+    }
+
+    @Override
+    public void display() {
+            System.out.println(("I'm a real Mallar duck"));
+    }
+}
